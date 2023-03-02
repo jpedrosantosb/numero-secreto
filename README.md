cea8bf00783178cad753f21499523dab985ccd3b
João Pedro Bastos <jpedrosantosb@gmail.com>
2023-03-01 22:20:04 -0300

    lógica do jogo e validações

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - Reconhecimento de voz com JavaScript
 - Manipulação de dados, eventos e elementos com JavaScript
 
+![numero-secreto](https://user-images.githubusercontent.com/69656085/222305816-ab78d547-ed13-41ca-bd61-c6e8a524ddb5.png)
+![numero-secreto1](https://user-images.githubusercontent.com/69656085/222305852-da49e62c-2dca-48b2-9cfc-b603fe1314de.png)
---
+++ app/validacao.js
@@ -0,0 +1,32 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return
+    }
+
+    if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor Inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto é ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>`
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
